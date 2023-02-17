5fdf83c4044ffb5ef281c2396e86c8d07a1a2239
lexi <kindaconfusion@gmail.com>
2023-02-17 10:28:03 -0600

    this code SUCKS

--- PA-3-Group-12-Gapud-Miller-Petry/PA-3-Source.cpp
+++ PA-3-Group-12-Gapud-Miller-Petry/PA-3-Source.cpp
@@ -21,34 +21,32 @@ int dynamicFib(int n) {
     v.push_back(0);
     v.push_back(1);
     for (int i=2; i<=n; i++) {
-        v[i] = v[i-1] + v[i-2];
+        v.push_back(v[i-1] + v[i-2]);
     }
     return v[n];
 }
 
+int test(int n, std::vector<int>& list1, std::vector<int>& list2, std::fstream& file) {
+    typedef std::chrono::high_resolution_clock Clock;
+    auto recurStart = Clock::now();
+    list1.push_back(recursiveFib(n));
+    auto recurStop = Clock::now();
+    auto recurElapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(recurStop - recurStart).count();
+    auto dynStart = Clock::now();
+    list2.push_back(dynamicFib(n));
+    auto dynStop = Clock::now();
+    auto dynElapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(dynStop - dynStart).count();
+    file << n << "," << list1.back() << "," << recurElapsed << "," << dynElapsed << ",tbd,tbd\n";
+    return list1.back();
+}
+
 int main() {
-    std::ofstream file;
+    std::fstream file;
     file.open("Fibonacci_Time.csv");
     std::string header = "n,f(n),T1: Time spent on the recursive algorithm (nanoseconds),T2: Time spent on the DP algorithm (nanoseconds),Value of (2^n)/n,Value of T1/T2\n";
     file << header;
-    const int ARRAY_SIZE = 20;
-    int recursive[ARRAY_SIZE];
-    int dynamic[ARRAY_SIZE];
-    std::stringstream csv;
-    for (int i=0; i<ARRAY_SIZE; i++) {
-        typedef std::chrono::high_resolution_clock Clock;
-        auto recurStart = Clock::now();
-        recursive[i] = recursiveFib(i);
-        auto recurStop = Clock::now();
-        auto recurElapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(recurStop - recurStart).count();
-        auto dynStart = Clock::now();
-        dynamic[i] = recursiveFib(i);
-        auto dynStop = Clock::now();
-        auto dynElapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(dynStop - dynStart).count();
-        csv << i << "," << recursive[i] << "," << recurElapsed << "," << dynElapsed << ",tbd,tbd\n";
-    }
-    file << csv.str();
-    file.close();
+    std::vector<int> recursive;
+    std::vector<int> dynamic;
     std::string input;
     while (true) {
         std::cout << "Use recursive algorithm or dynamic programming algorithm, or exit?\n";
@@ -59,20 +57,19 @@ int main() {
         }
         std::cout << "Enter an index.\n";
         std::cin >> input;
-
         try {
             int n = std::stoi(input); // Attempt to convert input to int.
             if (n < 0) {
                 std::cout << "Invalid option";
                 continue;
             }
-            int result = dynamic[n];
+            int result = test(n, recursive, dynamic, file);
             std::cout << "Index " << n << " in the Fibonacci sequence is: " << result << std::endl;
         }
         catch (std::invalid_argument e) {
             std::cout << "Invalid option.\n";
         }
-
+        file.close();
     }
 }
 
