ee1fe71a563c9af71cf91972a4fbcb2e2098ca85
Justin Petry <justincpetry@gmail.com>
2023-03-17 13:16:36 -0500

    merge fuck.cpp into main

--- PA-5-Group-12-Gapud-Miller-Petry/PA-5-Source.cpp
+++ PA-5-Group-12-Gapud-Miller-Petry/PA-5-Source.cpp
@@ -1,56 +1,26 @@
-// PA-4-Source.cpp
+// PA-5-Source.cpp
 
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <limits>
 #include <vector>
-#include <ctime>
-#include <optional>
+#include <limits>
 
 struct Task {
     int pay;
-    std::tm startTime;
-    std::tm endTime;
+    int startTime;
+    int endTime;
+    void print() { std::cout << "Pay is "<<pay<< " startTime is " << startTime << " endTime is " << endTime << std::endl; }
 };
 
-// Convert string time into the C++ time struct
-std::tm parseTime(std::string str) {
-    std::tm time{};
-    int hour;
-    int minute;
-    if (std::count(str.begin(), str.end(), ':') != 1)
-        return (std::tm){-1};
-    try {
-        hour = std::stoi(str.substr(0, str.find(':')));
-        minute = std::stoi(str.substr(str.find(':') + 1));
-    }
-    catch(std::exception) {
-        return (std::tm){-1};
-    }
-    time.tm_hour = hour;
-    time.tm_min = minute;
-    return time;
-
-}
-
-// Returns true if gt > lt, otherwise returns false.
-bool compareTime(std::tm gt, std::tm lt) {
-    if (gt.tm_hour > lt.tm_hour)
-        return true;
-    else if (gt.tm_min > lt.tm_min)
-        return true;
-    else
-        return false;
-}
-
-bool compareStartTime(Task lt, Task gt) {
-    return compareTime(lt.startTime, gt.startTime);
+bool compareTasks(Task *a, Task* b) {
+    if (a->endTime < b->endTime) return true;
+    return false;
 }
 
 int main() {
     int numTasks = 0;
-    std::vector<Task> tasks;
+    std::vector<Task*> tasks;
     std::string inputString;
     std::string divider = "     =============================================================================\n";
     while (true) {
@@ -78,37 +48,25 @@ int main() {
             continue;
         }
         std::cout << divider << "\tYou will now enter the pay and duration of each task.\n"
-            << "\tPlease enter the salary, then the start time, then the start time, and finally the end time.\n"
-            << "\tEnter all times in HH:MM format.\n"
-            << "\tPress enter after each input.\n" << divider;
+                  << "\tPlease enter the salary, then the start time, then the start time, and finally the end time.\n"
+                  << "\tEnter all times as integers between 0 and 512.\n"
+                  << "\tPress enter after each input.\n" << divider;
         for (int i = 0; i < numTasks; i++) {
-            Task task;
-            //int pay = 0;
+            Task * task = new Task;
             std::string start;
             std::string end;
-            std::cout << "\tTask " << i+1 << std::endl;
+            std::cout << "\tTask " << i + 1 << std::endl;
             std::cout << "\tWhat is this task's payment?\n";
-            std::cin >> task.pay;
+            std::cin >> task->pay;
             while (true) {
                 std::cout << "\tWhat time does this task start?\n";
-                while (true) {
-                    std::cin >> start;
-                    task.startTime = parseTime(start);
-                    // If successfully parsed, we will continue to the next input.
-                    // If parse failed, try again.
-                    if (task.startTime.tm_sec != -1)
-                        break;
-                    std::cout << "\tInvalid format, please try again.\n";
-                }
+                std::cin >> start;
+
                 std::cout << "\tWhat time does this task end?\n";
-                while (true) {
-                    std::cin >> end;
-                    task.endTime = parseTime(end);
-                    if (task.endTime.tm_sec != -1)
-                        break;
-                    std::cout << "\tInvalid format, please try again.\n";
-                }
-                if (compareTime(task.endTime, task.startTime))
+                std::cin >> end;
+                task->endTime = std::stoi(end);
+                task->startTime = std::stoi(start);
+                if (task->endTime > task->startTime)
                     break;
                 std::cout << "\tEnd time must be after start time.\n";
             }
@@ -116,7 +74,11 @@ int main() {
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             tasks.push_back(task);
         }
-        // Sort tasks by their start time
-        std::sort(tasks.begin(), tasks.end(), compareStartTime);
+        // Sort tasks by their END time, least to greatest
+        std::sort(tasks.begin(), tasks.end(), compareTasks);
+
+        for (int i = 0; i < tasks.size(); i++) {
+            tasks.at(i)->print();
+        }
     }
 }
