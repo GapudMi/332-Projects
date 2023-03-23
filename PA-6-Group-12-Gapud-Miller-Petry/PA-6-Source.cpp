624a64a39672fc47ae23fb3932042454038e2bff
Justin Petry <justincpetry@gmail.com>
2023-03-23 17:54:40 -0500

    it works lmao

--- PA-6-Group-12-Gapud-Miller-Petry/PA-6-Source.cpp
+++ PA-6-Group-12-Gapud-Miller-Petry/PA-6-Source.cpp
@@ -6,7 +6,7 @@
 struct cell {
     int value = -1;
     std::pair<int, int> location = std::make_pair(-1, -1); // row, col pair denotes location in matrix
-    int previous[3]; // A cell can have up to 3 cells leading into it.
+    cell* previous[3]; // A cell can have up to 3 cells leading into it.
 };
 
 bool operator== (const cell& a, const cell& b) {
@@ -49,18 +49,52 @@ int main() {
         checkForExit(sequenceTwo);
 
         // initialize 2D array :3
-        int colCount = sequenceOne.length();
-        int rowCount = sequenceTwo.length();
+        int cols = sequenceOne.length() + 1;
+        int rows = sequenceTwo.length() + 1;
 
-        // create matrix cell[rowCount][colCount]
-        cell** matrix = new cell*[rowCount];
-        for(int row = 0; row < rowCount; row++) {
-            matrix[row] = new cell[colCount];
+        // create matrix cell[rows][cols]
+        cell** matrix = new cell*[rows];
+        for(int row = 0; row < rows; row++) {
+            matrix[row] = new cell[cols];
             matrix[row][0] = cell{row*-1, std::make_pair(row, 0), NULL};
         }
-        for (int col = 0; col < colCount; col++) {
+        for (int col = 0; col < cols; col++) {
             matrix[0][col] = cell{col*-1, std::make_pair(0, col), NULL};
         }
-        printMatrix(matrix, rowCount, colCount);
+
+        for (int row = 1; row < rows; row++) {
+            for (int col = 1; col < cols; col++) {
+                cell c = {};
+                int ptr = 0;
+                int bestScore = 0;
+                if (sequenceOne[col-1] == sequenceTwo[row-1]) {
+                    c.previous[0] = &matrix[row-1][col-1];
+                    ptr++;
+                    bestScore = matrix[row-1][col-1].value + 5;
+                }
+                cell* up = &matrix[row-1][col];
+                cell* left = &matrix[row][col-1];
+                if (left->value == up->value) {
+                    if (matrix[row][col-1].value >= bestScore) {
+                        bestScore = left->value-1;
+                        c.previous[ptr] = left;
+                        c.previous[ptr+1] = up;
+                    }
+                }
+                else {
+                    cell* prev = (left->value > up->value) ? left : up;
+                    if (prev->value >= bestScore) {
+                        //c.value = prev->value - 1;
+                        bestScore = prev->value - 1;
+                        c.previous[ptr] = prev;
+                    }
+
+                }
+                c.value = bestScore;
+                c.location = std::make_pair(row, col);
+                matrix[row][col] = c;
+            }
+        }
+        printMatrix(matrix, rows, cols);
     }
 }
