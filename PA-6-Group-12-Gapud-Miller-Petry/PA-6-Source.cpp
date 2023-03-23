462eb3c77b4b85b9898cebcaea58f3cf94bca5d8
lexi <justincpetry@gmail.com>
2023-03-23 16:19:33 -0500

    rudimentary print function. idk. i'm tired.

--- PA-6-Group-12-Gapud-Miller-Petry/PA-6-Source.cpp
+++ PA-6-Group-12-Gapud-Miller-Petry/PA-6-Source.cpp
@@ -4,10 +4,14 @@
 #include <vector>
 
 struct cell {
-    int value;
-    std::pair<int, int> location; // row, col pair denotes location in matrix
+    int value = -1;
+    std::pair<int, int> location = std::make_pair(-1, -1); // row, col pair denotes location in matrix
     int previous[3]; // A cell can have up to 3 cells leading into it.
 };
+
+bool operator== (const cell& a, const cell& b) {
+    return ((a.location.first == b.location.first) && (a.location.second == b.location.second));
+}
 
 void checkForExit(std::string input) {
     std::transform(input.begin(), input.end(), input.begin(),
@@ -15,17 +19,31 @@ void checkForExit(std::string input) {
     if (input == "exit") exit(0);
 }
 
+void printMatrix(cell** mat, int rows, int cols) {
+    // in future this will also print arrows and shit idk
+    std::cout << " ";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (mat[i][j] == cell{{}})  {
+                continue;
+            }
+            else std::cout << mat[i][j].value << "     ";
+        }
+        std::cout << "\n\n";
+    }
+}
+
 int main() {
     std::string divider = "     =============================================================================\n";
-    std::cout << divider;
     while (true) {
+        std::cout << divider;
         std::cout << "\tWelcome. Please enter your first sequence, then press enter.\n"
                   << "\tA sequence is a combination of letters and gaps (denoted by an underscore).\n"
                   << "\tAlternatively, type \"exit\" and press enter to quit at any time.\n" << divider;
         std::string sequenceOne;
         std::cin >> sequenceOne;
         checkForExit(sequenceOne);
-        std::cout << "\tEnter your second sequence.";
+        std::cout << "\tEnter your second sequence.\n";
         std::string sequenceTwo;
         std::cin >> sequenceTwo;
         checkForExit(sequenceTwo);
@@ -43,5 +61,6 @@ int main() {
         for (int col = 0; col < colCount; col++) {
             matrix[0][col] = cell{col*-1, std::make_pair(0, col), NULL};
         }
+        printMatrix(matrix, rowCount, colCount);
     }
 }
